9e89ac96733dd39bdb0f053a8588dce3dbf8eebe
ti777777 <ti12272198686@yahoo.com.tw>
2025-09-04 09:01:12 +0800

    tweak usermenu style

--- web/src/components/usermenu/UserMenu.tsx
+++ web/src/components/usermenu/UserMenu.tsx
@@ -5,6 +5,7 @@ import { useNavigate } from "react-router-dom"
 import { signOut } from "../../api/auth"
 import { useSidebar } from "../sidebar/SidebarProvider"
 import { useTranslation } from "react-i18next"
+import { twMerge } from "tailwind-merge"
 
 const UserMenu= () => {
     const {t} =useTranslation()
@@ -35,11 +36,9 @@ const UserMenu= () => {
 
         <DropdownMenu.Portal>
             <DropdownMenu.Content
-                className="z-50 rounded-md  bg-white text-gray-900 dark:bg-neutral-900 dark:text-gray-100 dark:border dark:border-gray-100 p-[5px] shadow-[0px_10px_38px_-10px_rgba(22,_23,_24,_0.35),_0px_10px_20px_-15px_rgba(22,_23,_24,_0.2)] will-change-[opacity,transform] data-[side=bottom]:animate-slideUpAndFade data-[side=left]:animate-slideRightAndFade data-[side=right]:animate-slideLeftAndFade data-[side=top]:animate-slideDownAndFade"
+                className={twMerge(isCollapse ? "":"w-[260px]"," z-50 rounded-md  bg-white text-gray-900 dark:bg-neutral-900 dark:text-gray-100 dark:border dark:border-gray-100 p-[5px] shadow-[0px_10px_38px_-10px_rgba(22,_23,_24,_0.35),_0px_10px_20px_-15px_rgba(22,_23,_24,_0.2)] will-change-[opacity,transform] data-[side=bottom]:animate-slideUpAndFade data-[side=left]:animate-slideRightAndFade data-[side=right]:animate-slideLeftAndFade data-[side=top]:animate-slideDownAndFade")}
                 side={!isOver1280 || !isCollapse ? "top" : "right"} 
                 sideOffset={5} 
-                align={!isOver1280 || !isCollapse ? "start" : "end"}
-                alignOffset={0}  
             >
                 <DropdownMenu.Item className="  select-none rounded-lg leading-none outline-none data-[disabled]:pointer-events-none data-[highlighted]:bg-gray-100 dark:data-[highlighted]:text-gray-900 ">
                     <button onClick={handleLogout} className="flex gap-3 p-3 items-center w-full" >
