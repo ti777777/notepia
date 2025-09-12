853a25b0f53598b8c84caf17672a5bac97a49744
ti777777 <a104221087@gmail.com>
2025-09-12 20:36:18 +0800

    feat: integrate TextInput and SubmitButton components in WorkspaceSetupPage for improved form handling (#44)

--- web/src/components/textinput/TextInput.tsx
+++ web/src/components/textinput/TextInput.tsx
@@ -5,6 +5,7 @@ interface Props {
     type?: string
     value: string
     title: string
+    placeholder?: string 
     onChange: (e: ChangeEvent<HTMLInputElement>) => void
     required?: boolean
     className?: string
@@ -12,11 +13,12 @@ interface Props {
 
 const TextInput: FC<Props> = (props: Props) => {
     return <input
-        className={props.className ?? "appearance-none focus:ring-2 focus:ring-orange-600 rounded w-full py-2 px-3 text-gray-700 dark:text-gray-300 dark:bg-stone-800 leading-tight focus:outline-none focus:shadow-outline"}
+        className={props.className ?? "appearance-none bg-white dark:text-gray-900 focus:ring-2 focus:ring-orange-600 rounded w-full py-2 px-3  leading-tight focus:outline-none focus:shadow-outline"}
         id={props.id}
         type={props.type ?? "text"}
         value={props.value}
         title={props.title}
+        placeholder={props.placeholder}
         onChange={props.onChange}
         required={props.required ?? true}
     />
--- web/src/pages/WorkspaceSetup/WorkspaceSetupPage.tsx
+++ web/src/pages/WorkspaceSetup/WorkspaceSetupPage.tsx
@@ -2,10 +2,12 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { createWorkspace } from "../../api/workspace";
 import { useTranslation } from "react-i18next";
+import SubmitButton from "../../components/submitbutton/SubmitButton";
+import TextInput from "../../components/textinput/TextInput";
 
 const WorkspaceSetupPage = () => {
     const [workspaceName, setWorkspaceName] = useState("");
-    const {t} = useTranslation()
+    const { t } = useTranslation()
     const navigate = useNavigate()
 
     const handleCreate = async () => {
@@ -14,10 +16,15 @@ const WorkspaceSetupPage = () => {
         navigate("/")
     };
 
+    const handleSubmit = (e: React.FormEvent) => {
+        e.preventDefault();
+        handleCreate();
+    };
+
     return (
-        <div className="w-full min-h-dvh  flex flex-col">
+        <div className="w-full min-h-screen  flex flex-col">
             <div className="grow flex justify-center items-center">
-                <div className="flex flex-col gap-3">
+                <form onSubmit={handleSubmit} className="flex flex-col gap-3">
                     <div className="flex flex-col gap-3 p-5">
                         <div className="text-3xl font-bold text-center">
                             {t("pages.workspaceSetup.createWorkspace")}
@@ -26,26 +33,24 @@ const WorkspaceSetupPage = () => {
                             {t("pages.workspaceSetup.pleaseEnterYourWorkspaceName")}
                         </div>
                     </div>
-                    <div className="flex flex-col gap-3 p-6 shadow-xl rounded-xl dark:border">
+                    <div className="flex flex-col gap-3 p-6 w-80">
                         <label className="text-sm font-medium" htmlFor="workspace-name">
                             {t("pages.workspaceSetup.workspaceName")}
                         </label>
-                        <input
+                        <TextInput
                             id="workspace-name"
                             value={workspaceName}
+                            title={"workspace-name"}
                             onChange={e => setWorkspaceName(e.target.value)}
                             placeholder={t("pages.workspaceSetup.workspaceNamePlaceholder")}
-                            className="appearance-none border rounded-lg w-64 max-w-full p-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                         />
-                        <button
-                            className=" bg-amber-600 hover:bg-amber-700 text-white text-base font-bold py-2 px-4 rounded-lg focus:outline-none focus:shadow-outline disabled:opacity-50"
-                            onClick={handleCreate}
+                        <SubmitButton
                             disabled={!workspaceName.trim()}
                         >
                             {t("actions.create")}
-                        </button>
+                        </SubmitButton>
                     </div>
-                </div>
+                </form>
             </div>
         </div>
     );
