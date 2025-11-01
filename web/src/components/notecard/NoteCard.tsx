c1b611cf74718773c2a2784faa95339d6d94bb8f
ti777777 <a104221087@gmail.com>
2025-11-01 23:39:43 +0800

    refactor: simplify NoteCard component by removing optional props and enhancing link usage (#138)

--- web/src/components/notecard/NoteCard.tsx
+++ web/src/components/notecard/NoteCard.tsx
@@ -1,35 +1,26 @@
 import { FC } from "react"
 import { Link } from "react-router-dom"
-import { MoveDiagonal } from "lucide-react"
 import { NoteData } from "@/api/note"
 import NoteTime from "../notetime/NoteTime"
 import FullNote from "../fullnote/FullNote"
 
 interface NoteCardProps {
     note: NoteData
-    linkTo?: string
-    showLink?: boolean
+    linkTo: string
 }
 
-const NoteCard: FC<NoteCardProps> = ({ note, linkTo, showLink = true }) => {
+const NoteCard: FC<NoteCardProps> = ({ note, linkTo }) => {
     return (
-        <div className="bg-white dark:bg-neutral-800 border sm:shadow-sm dark:border-neutral-600 rounded-lg overflow-auto flex flex-col gap-2 py-4">
+        <Link to={linkTo} className="bg-white dark:bg-neutral-800 border sm:shadow-sm dark:border-neutral-600 rounded-lg overflow-auto flex flex-col gap-2 py-4">
             <div className="flex justify-between text-gray-500 px-4">
                 <div>
                     <NoteTime time={note.updated_at ?? ""} />
                 </div>
-                {showLink && linkTo && (
-                    <div>
-                        <Link to={linkTo}>
-                            <MoveDiagonal size={16} />
-                        </Link>
-                    </div>
-                )}
             </div>
             <div className="break-all w-full flex flex-col m-auto">
                 <FullNote note={note} />
             </div>
-        </div>
+        </Link>
     )
 }
 
